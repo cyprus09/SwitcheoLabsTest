905bee680513218b415947c9319931b464e259ec
Mayank Pallai <mayankpallai@gmail.com>
2023-09-25 03:44:32 +0800

    added script.js file to problem2

---
+++ src/problem2/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Define token prices (you can fetch this data from an API)
+    const tokenPrices = {
+      ETH: 3000,
+      BTC: 45000,
+      USDT: 1,
+    };
+  
+    // Get DOM elements
+    const form = document.querySelector("form");
+    const inputAmount = document.getElementById("input-amount");
+    const outputAmount = document.getElementById("output-amount");
+    const swapButton = document.querySelector("button");
+  
+    // Function to calculate the output amount based on the input amount and selected tokens
+    const calculateOutputAmount = () => {
+      const inputToken = "ETH"; // You can implement token selection logic
+      const outputToken = "BTC"; // You can implement token selection logic
+      const inputValue = parseFloat(inputAmount.value) || 0;
+      const exchangeRate = tokenPrices[outputToken] / tokenPrices[inputToken];
+      const calculatedOutput = inputValue * exchangeRate;
+      return calculatedOutput.toFixed(2);
+    };
+  
+    // Function to update the output amount when the input amount changes
+    const updateOutputAmount = () => {
+      const calculatedOutput = calculateOutputAmount();
+      outputAmount.value = calculatedOutput;
+    };
+  
+    // Event listener for input amount changes
+    inputAmount.addEventListener("input", updateOutputAmount);
+  
+    // Event listener for form submission
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+  
+      // Perform the currency swap here (you can make an API request)
+  
+      // Show a success message or handle errors
+      alert("Swap completed successfully!");
+    });
+  
+    // Initial update of the output amount
+    updateOutputAmount();
+  });
+  
